70f64ab5d2ad2473bb672d3d393ccb3eef9e5e13
Rushin Shah <58971902+RushinShah22@users.noreply.github.com>
2024-07-09 15:04:52 +0530

    test deployment

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 from pymongo.database import Database
+print("hello from db")
 client=MongoClient("mongodb+srv://dppatel:Admin1234@cluster0.ugxqzd8.mongodb.net/")
 db=client.get_database("users_db")
 try:
